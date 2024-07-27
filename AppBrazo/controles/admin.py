700258524eab06bf6e3fc73bc0e43fde128090d4
Omar <marco.chiliguano@unl.edu.ec>
2024-07-27 16:19:24 -0500

    controles

--- AppBrazo/controles/admin.py
+++ AppBrazo/controles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ServoData
 
-# Register your models here.
+@admin.register(ServoData)
+class ServoDataAdmin(admin.ModelAdmin):
+    list_display = ('esp32_ip', 'timestamp', 'base', 'hombro', 'muneca', 'codo', 'pinza', 'camera', 'forward', 'backward', 'left', 'right')
+    readonly_fields = ('timestamp',)
+
+
